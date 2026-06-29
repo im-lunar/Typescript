d0752424b968f7107f3111b12b8e65f908477452
im-lunar <itslunarwrld@gmail.com>
2026-06-29 11:05:54 +0530

    ts intro

--- src/a.ts
+++ src/a.ts
@@ -1,5 +1,37 @@
+// Learn how to give types to function arguments in TypeScript
+
 function greet(firstName: string) {
     console.log("Hello ", firstName);
 }
 
 greet("kaur");
+
+
+// Learn how to assign a return type to a function in TypeScript
+function sum(a: number, b: number) {
+    return a + b;
+}
+
+const value = sum(1,2);
+console.log(value);
+
+
+// Understand Type Inference in TypeScript
+function isLegal(age: number) : boolean {
+    if (age > 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(isLegal(22));
+
+// Learn to work with functions as parameters in TypeScript
+function delayedFunction(fn: () => void) {
+    setTimeout(fn, 1000);
+}
+
+delayedFunction(function () {
+    console.log("hi there");
+});
