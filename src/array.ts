912624bd235c66c3eb56f0d67f57375bdb464f1b
im-lunar <itslunarwrld@gmail.com>
2026-06-30 12:37:18 +0530

    arrays done

--- src/array.ts
+++ src/array.ts
@@ -14,3 +14,25 @@ function maxValue(arr: number[]) {
 }
 
 console.log(maxValue([1,2,3]))
+
+// Given a list of users, filter out users that are legal (greater than 18 years of age)
+
+interface User {
+    firstName: string,
+    lastName: string,
+    age: number
+}
+
+function filteredUsers(users: User[]) {
+    return users.filter(x => x.age >= 18);
+}
+
+console.log(filteredUsers([{
+    firstName: "Reet",
+    lastName: "Kaur",
+    age: 21
+}, {
+    firstName: "Albedo",
+    lastName: "Y",
+    age: 17
+}]))
